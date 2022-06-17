f335fef499ed72b8a75150e0ae4f939aaddbd5d4
kornsour <ajkaiserauer@gmail.com>
2022-06-17 16:51:28 -0400

    summaries endpoints

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,6 +1,6 @@
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
-from typing import Union
+from typing import Union, List
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -13,7 +13,10 @@ async def post(payload: SummaryPayloadSchema) -> int:
 
 async def get(id: int) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).first().values()
-    
     if summary:
         return summary
     return None
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -23,10 +23,10 @@ def test_create_summaries_invalid_json(test_app):
     }
 
 def test_read_summary(test_app_with_db):
-    response = test_app_with_db.post("/sumaries/", data=json.dumps({"url": "https://foo.bar"}))
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
     summary_id = response.json()["id"]
 
-    response = test_app_with_db.get(f"/summaries/{summary_id}")
+    response = test_app_with_db.get(f"/summaries/{summary_id}/")
     assert response.status_code == 200
 
     response_dict = response.json()
@@ -34,3 +34,18 @@ def test_read_summary(test_app_with_db):
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"]
     assert response_dict["created_at"]
+
+def test_read_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.get("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+def test_read_all_summaries(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.get("/summaries/")
+    assert response.status_code == 200
+
+    response_list = response.json()
+    assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
